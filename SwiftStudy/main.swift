e7191ba7bee8d2850d2f53258f7a09d0d3f787ab
dannykim1215 <minzami1215@gmail.com>
2024-01-21 18:02:36 +0900

    study: 1. 삼항조건연산자

--- SwiftStudy/main.swift
+++ SwiftStudy/main.swift
@@ -2,10 +2,23 @@
 //  main.swift
 //  SwiftStudy
 //
-//  Created by 김민제 on 1/17/24.
+//  Created by danny on 1/17/24.
 //
 
-import Foundation
+//삼항조건연산자
+var valueA: Int = 10
+var valueB: Int = 5
+var biggerValue: Int = valueA > valueB ? valueA : valueB
+print(biggerValue) // 10
 
-print("Hello, World!")
+valueA = 0
+valueB = -3
+biggerValue = valueA > valueB ? valueA : valueB
+print(biggerValue) // 0
 
+var stringA: String = ""
+var stringB: String = "String"
+var resultValue: Double = stringA.isEmpty ? 1.0 : 0.0
+print(resultValue) // 1.0
+resultValue = stringB.isEmpty ? 1.0 : 0.0
+print(resultValue) // 0.0
